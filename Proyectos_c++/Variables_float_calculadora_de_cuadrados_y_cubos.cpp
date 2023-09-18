d08a6d16d5b7ee9d2a6284fb0268e88333564361
Jaredow604 <143060313+Jaredow604@users.noreply.github.com>
2023-09-18 17:27:36 -0600

    Add files via upload

---
+++ Proyectos_c++/Variables_float_calculadora_de_cuadrados_y_cubos.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+    double numero;
+    cout <<"Ingrese un numero: ";
+    cin >> numero;
+
+    double cuadrado = numero * numero;
+    double cubo = numero * numero * numero;
+
+    cout <<"El cuadrado de" << numero << "es" << cuadrado << end1;
+    cout <<"El cuadrado de" << numero << "es" << cubo << end1;
+    
