ffba7b6be629b97b1853704fc231b7c9ad269627
Jaredow604 <143060313+Jaredow604@users.noreply.github.com>
2023-10-07 21:56:13 -0600

    Add files via upload

---
+++ Proyectos_python/menu_if_else.py
@@ -0,0 +1,23 @@
+print ("Puedo calcular areas de diferentes figuras, cuadrado, triangulo y circulo.")
+print ("selecciona una opcion)
+print ("1.Area de cuadrado\n2.Area de triangulo\n3.Area de circulo")
+opcion=int(input())
+if opcion == 1: 
+    print("Ingresa la medida de un lado de tu cuadrado")
+    lado=int(input())
+    Area_cuadrado=lado*lado
+    print(f"El area de tu cuadrado es:{Area_cuadrado}")
+elif opcion == 2:
+    print("Infresa la base de tu triangulo")
+    base=int(input())
+    print("Ingresa la altura de tu triangulo")
+    altura=int(input())
+    Area_triangulo=(base*altura)/2
+    print(f"El area de tu triangulo es:{Area_triangulo}")
+elif opcion == 3:
+    print("Ingresa el radio de tu circulo")
+    pi=3.14159265359
+    radio=int(input())
+    Area_circulo=pi*(radio*radio)
+    print(f"El area de tu circulo es{Area_circulo} ")
+    
