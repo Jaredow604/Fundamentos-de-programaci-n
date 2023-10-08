764334deaced56d6ff3912fa2794a8e2d6cf6d8e
Jaredow604 <143060313+Jaredow604@users.noreply.github.com>
2023-10-07 22:40:31 -0600

    Add files via upload

---
+++ Proyectos_c++/menu_if_else.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int base;
+    int altura;
+    int radio;
+    int lado;
+    float pi = 3.14159265359;
+
+    cout << "Hola! puedo calcular el área de tres figuras distintas, triángulo, círculo y cuadrado." << endl;
+    cout << "Selecciona una figura" << endl;
+    cout << "1. Triángulo\n2. Círculo\n3. Cuadrado\n";
+
+    int opcion;
+    cin >> opcion;
+
+    if (opcion == 1) {
+        cout << "Ingresa la altura del triángulo" << endl;
+        cin >> altura;
+        cout << "Ingresa la base del triángulo" << endl;
+        cin >> base;
+        int AreaDeTriangulo = (base * altura) / 2;
+        cout << "El área de tu triángulo es: " << AreaDeTriangulo << endl;
+    } else if (opcion == 2) {
+        cout << "Ingresa el radio del círculo" << endl;
+        cin >> radio;
+        float AreaDeCirculo = pi * pow(radio, 2);
+        cout << "El área de tu círculo es: " << AreaDeCirculo << endl;
+    } else if (opcion == 3) {
+        cout << "Ingresa la medida de uno de los lados del cuadrado" << endl;
+        cin >> lado;
+        int AreaDeCuadrado = lado * lado;
+        cout << "El área de tu cuadrado es: " << AreaDeCuadrado << endl;
+    }
+
+    return 0;
+}
